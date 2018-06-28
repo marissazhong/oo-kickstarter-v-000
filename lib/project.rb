9f7dc528842c051bb4e41cf584923674f0f5516d
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 22:55:17 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,13 @@
 class Project
   attr_accessor :project_name
+  
   def initialize(project_name)
     @project_name = project_name
+    @backers = []
   end
+  
   def backers
-
+    
   end
+  
 end
