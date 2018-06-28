2141f61e3afcb5f5c4b3e8e01ddcbd7fcb1cd8c9
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 23:32:53 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,24 +8,12 @@ class Backer
     @backed_projects << project
     #Project.add_backer(self)
   end
-  def backed_projects
+  def backed_projects=(projects)
     @backed_projects
   end
 end
 
-class Project
-  attr_accessor :title, :backers
-  def initialize(project_name)
-    @title = project_name
-    @backers = []
-  end
-  def backers
-    @backers
-  end
-  def self.add_backer(backer)
-    @backers << backer
-  end
-end
+
 
 backer = Backer.new("Avi")
 spencer = Backer.new("Spencer")
